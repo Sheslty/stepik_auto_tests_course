51bd0a3cb35d9e48846d930af741be7193096d99
Sheeshelty <ASM0@inbox.ru>
2023-01-17 05:16:41 +0300

    rename all .py files

--- some_tasks_with_selenium/script.py
+++ some_tasks_with_selenium/script.py
@@ -1,36 +1,28 @@
-import time
-
-# webdriver это и есть набор команд для управления браузером
 from selenium import webdriver
-
-# импортируем класс By, который позволяет выбрать способ поиска элемента
 from selenium.webdriver.common.by import By
+import time 
 
-# инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
-driver = webdriver.Chrome()
+link = "http://suninjuly.github.io/simple_form_find_task.html"
 
-# команда time.sleep устанавливает паузу в 5 секунд, чтобы мы успели увидеть, что происходит в браузере
-time.sleep(5)
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
 
-# Метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
-driver.get("https://suninjuly.github.io/text_input_task.html")
-time.sleep(5)
+    input1 = browser.find_element(By.TAG_NAME, "input")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, "last_name")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CLASS_NAME, "city")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("Russia")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
 
-# Метод find_element позволяет найти нужный элемент на сайте, указав путь к нему. Способы поиска элементов мы обсудим позже
-# Метод принимает в качестве аргументов способ поиска и значение, по которому мы будем искать
-# Ищем поле для ввода текста
-textarea = driver.find_element(By.CSS_SELECTOR, ".textarea")
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
 
-# Напишем текст ответа в найденное поле
-textarea.send_keys("get()")
-time.sleep(5)
-
-# Найдем кнопку, которая отправляет введенное решение
-submit_button = driver.find_element(By.CSS_SELECTOR, ".submit-submission")
-
-# Скажем драйверу, что нужно нажать на кнопку. После этой команды мы должны увидеть сообщение о правильном ответе
-submit_button.click()
-time.sleep(5)
-
-# После выполнения всех действий мы должны не забыть закрыть окно браузера
-driver.quit()
+# не забываем оставить пустую строку в конце файла
